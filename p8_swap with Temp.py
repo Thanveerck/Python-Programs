b96f06dd38f370f4688bec5074c87c9eb18b015c
Thanveer Sha <thanveer09.ck@gmail.com>
2020-11-19 17:18:05 +0400

    python programs

---
+++ p8_swap with Temp.py
@@ -0,0 +1,12 @@
+number1 = 2
+number2 = 3
+temp = 0;
+
+
+print('values before swapping {0},{1}'.format(number1,number2))
+
+temp = number1
+number1 = number2
+number2 = temp
+
+print('values after swappng {0},{1}'.format(number1,number2))
